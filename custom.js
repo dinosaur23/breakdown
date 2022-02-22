6c28d8665fe82e58a44ae7e841278efc4f2af4ff
dinosaur23 <96414278+dinosaur23@users.noreply.github.com>
2022-02-23 10:15:32 +1100

    Create custom.js

---
+++ custom.js
@@ -0,0 +1,11 @@
+firebase.auth().signInWithCustomToken(token)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
